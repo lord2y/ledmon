8f644f588a7116f0e11e139c12473ed5230852cf
Mariusz Tkaczyk <mariusz.tkaczyk@linux.intel.com>
2021-09-03 13:35:21 +0200

    Revert "Remove possibility to add NULL item into the list (#82)"
    This reverts commit 45eb54a2e64358d8bb1c72f5a56916f081f45d83.

--- src/config_file.c
+++ src/config_file.c
@@ -240,8 +240,11 @@ static char *conf_list_to_str(struct list *list)
 
 	memset(buf, 0, sizeof(buf));
 	list_for_each(list, elem) {
-		int curr = strlen(buf);
-		snprintf(buf + curr, sizeof(buf) - curr, "%s,", elem);
+		if (elem) {
+			int curr = strlen(buf);
+
+			snprintf(buf + curr, sizeof(buf) - curr, "%s,", elem);
+		}
 	}
 
 	return str_dup(buf);
